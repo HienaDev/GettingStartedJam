f432858ff4ef78ab78c38947471932ca4fca3bf3
HienaDev <ant.rodrigues1999@gmail.com>
2025-07-13 17:47:31 +0100

    Add condutive logic

--- Assets/ConductiveItem.cs
+++ Assets/ConductiveItem.cs
@@ -4,14 +4,24 @@ using System.Collections.Generic;
 public class ConductiveItem : MonoBehaviour
 {
     [SerializeField] private Renderer[] pointsToPower;
-    private bool poweredOn = false;
+    [SerializeField] private bool poweredOn = false;
+
+    private List<ConductiveItem> poweringMeOn;
+
+    [SerializeField] private bool isPowerSource = false;
+
+    public bool PoweredOn
+    {
+        get { return poweredOn; }
+        set { TogglePower(value); }
+    }
 
     [SerializeField] private Material emissiveMaterial;
     private List<Material> defaultMaterial;
-    public void TogglePower(bool toggle)
+    private void TogglePower(bool toggle)
     {
-        poweredOn = toggle;
-        if (poweredOn)
+        poweredOn = toggle || isPowerSource;
+        if (poweredOn || isPowerSource)
         {
             foreach (Renderer point in pointsToPower)
             {
@@ -40,6 +50,8 @@ public class ConductiveItem : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        poweringMeOn = new List<ConductiveItem>();
+
         foreach (Renderer point in pointsToPower)
         {
             if (point != null)
@@ -49,14 +61,48 @@ public class ConductiveItem : MonoBehaviour
                 defaultMaterial.Add(point.material);
             }
         }
-    }
 
+        if (poweredOn)
+        {
+            TogglePower(true); // Ensure the item starts in the correct state
+        }
+        else
+        {
+            TogglePower(false); // Ensure the item starts in the correct state
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P)) // Example key to toggle power
         {
             TogglePower(!poweredOn);
+        }
+    }
+
+    public void RegisterItem(ConductiveItem item)
+    {
+        if (!poweringMeOn.Contains(item))
+        {
+            poweringMeOn.Add(item);
+            Debug.Log($"{gameObject.name} registered to power on {item.gameObject.name}.");
+            if (!poweredOn)
+            {
+                TogglePower(true); // If this item is being powered on, turn it on
+            }
+        }
+    }
+
+    public void UnregisterItem(ConductiveItem item)
+    {
+        if (poweringMeOn.Contains(item))
+        {
+            poweringMeOn.Remove(item);
+            Debug.Log($"{gameObject.name} unregistered from powering on {item.gameObject.name}.");
+            if(poweringMeOn.Count <= 0)
+            {
+                TogglePower(false); // If no items are powering this one, turn it off
+            }
         }
     }
 }
